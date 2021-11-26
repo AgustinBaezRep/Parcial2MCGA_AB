aaed76aa33950e1325e8d224f3616384c41c0040
Agustin Baez <agustinbaez155@gmail.com>
2021-11-26 00:46:01 -0300

    redux and other probes

--- players/src/App.js
+++ players/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Nav from './components/navbar'
+import Body from './components/body';
+import Footer from './components/footer'
+import {Routes, BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      hola mundo {1+2}
-    </div>
+    <Router>
+      <div className="App">
+        <Nav></Nav  >
+        <Routes>
+          <Route path='/players' element={<Body/>}></Route>
+        </Routes>
+        <Footer></Footer>
+      </div>
+    </Router>
+    
   );
 }
 
